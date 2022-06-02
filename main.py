7217a615d491d46bf5f74bde7845b2a48d06f913
DusanKarel <dusanko@karlikovci.sk>
2022-06-02 20:58:05 +0200

    pridali sme novu dosku

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ width, height, background = 700, 500, "black"
 plocha = tkinter.Canvas(width=width, height=height, background=background)
 plocha.pack()
 
-doska = Doska(farba="green", rozmer_x=30, rozmer_y=5,  # vyroba dosky
-              speed=15, plocha=plocha, home_x=width / 2, home_y=height - 30, platno_width=width)
+doska_1 = Doska(farba="dark red", rozmer_x=30, rozmer_y=5,  # vyroba dosky
+                speed=15, plocha=plocha, home_x=width / 2, home_y=height - 30, platno_width=width)
 
-plocha.bind_all('<Right>', doska.move_right)  # nabidnovat si hybanie ta tlacitko
+plocha.bind_all('<Right>', doska_1.move_right)  # nabidnovat si hybanie ta tlacitko
 
-plocha.bind_all('<Left>', doska.move_left)  # nabidnovat si hybanie ta tlacitko
+plocha.bind_all('<Left>', doska_1.move_left)  # nabidnovat si hybanie ta tlacitko
+
+doska_2 = Doska(farba="dark blue", rozmer_x=30, rozmer_y=5,  # vyroba dosky
+                speed=15, plocha=plocha, home_x=width / 2, home_y= 30, platno_width=width)
+
+plocha.bind_all('<d>', doska_2.move_right)  # nabidnovat si hybanie ta tlacitko
+
+plocha.bind_all('<a>', doska_2.move_left)  # nabidnovat si hybanie ta tlacitko
 
 
 # Toto je potrebné aby aj keď zbehneš tento program cez PyCharm sa reálne všetko spojené s plochou zobrazilo
